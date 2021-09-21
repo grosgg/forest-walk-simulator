c6e3096971adab40712ad5179d599141fa7d593e
Jeremy Marin <marin.jeremy@gmail.com>
2021-09-21 22:58:37 +0900

    Add forest ground + AxesHelper + file-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import Grid from './Grid.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,18 +11,20 @@ const renderer = new THREE.WebGLRenderer( { canvas } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const controls = new OrbitControls( camera, renderer.domElement );
 
-camera.position.z = 5;
+const axesHelper = new THREE.AxesHelper(10);
+scene.add( axesHelper );
+
+const grid = new Grid;
+scene.add( grid.mesh );
+
+// camera.position.set(2, 1.8, 2);
+controls.target = new THREE.Vector3(10, 0, 20);
+controls.update();
 
 const animate = function () {
   requestAnimationFrame( animate );
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
 
   renderer.render( scene, camera );
 };
