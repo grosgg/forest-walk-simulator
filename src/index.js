1173190ad3a06d516ecb6824b951529db6faf503
Go Nakano <go@crowdcast.jp>
2021-09-21 17:39:38 +0900

    remove prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import * as THREE from "three";
-import { tent, entrance } from "./tent";
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const canvas = document.querySelector("#canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const canvas = document.querySelector('#canvas');
+const renderer = new THREE.WebGLRenderer( { canvas } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 // adding tent with entrance
 scene.add(tent);
@@ -25,10 +20,10 @@ scene.add(entrance);
 camera.position.z = 5;
 
 const animate = function () {
-  requestAnimationFrame(animate);
+  requestAnimationFrame( animate );
+
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
 
-animate();
+  renderer.render( scene, camera );
+};
