8563c2fe0fb795fbe9c198a305da93b330103cfb
Mike Warren <mikeywarren89@gmail.com>
2021-09-22 14:39:31 +0900

    Add chill music

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import ForestTexturePZ from './images/skybox/pz.jpg';
 import ForestTextureNX from './images/skybox/nx.jpg';
 import ForestTextureNY from './images/skybox/ny.jpg';
 import ForestTextureNZ from './images/skybox/nz.jpg';
+// Emile's WIP
+// import Music from './audio/Shithesizer_numero_deux.mp3';
+// Chill chiptune
+import Music from './audio/chill.mp3';
 
 import ConicalTree from './ConicalTree.js';
 
@@ -25,6 +29,23 @@ const controls = new OrbitControls( camera, renderer.domElement );
 const axesHelper = new THREE.AxesHelper(10);
 scene.add( axesHelper );
 
+// Audio
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( Music, function( buffer ) {
+  sound.setBuffer( buffer );
+  sound.setLoop( true );
+  sound.setVolume( 0.15 );
+  // comment out to stop when devving
+  sound.play();
+});
 
 const skyboxTexture = new THREE.CubeTextureLoader().load([
   ForestTexturePX,
